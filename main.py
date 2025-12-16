6dfc4ac9b3540a8706f26d2206e5c7307ee79431
Your NameDavid <ndddn87@gmail.com>
2025-12-16 23:01:13 +0100

    worked on gesture control. still incomplete

--- main.py
+++ main.py
@@ -3,31 +3,106 @@ import mediapipe as mp
 
 import pygame
 
+import threading
+
+
 class Movement_game():
+
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1280,720))
+        self.screen = pygame.display.set_mode((1280, 720))
+
         self.running = True
-        self.player_pos = pygame.Vector2(self.screen.get_width()/2,self.screen.get_height()/2)
+        self.player_pos = pygame.Vector2(
+            self.screen.get_width()/2, self.screen.get_height()/2)
         self.displacement = 10
+        self.cap = 0
+        self.displacementx =0
+        self.displacementy = 0
+        
 
-    # def player(self):
-    #     self.player_pos = pygame.Vector2(self.screen.get_width()/2,self.screen.get_height()/2)
-
-    def keyboard_movement(self):
+    def keyboard_control(self):
         keys = pygame.key.get_pressed()
-        if len(keys)>0:
+        if len(keys) > 0:
             if keys[pygame.K_w]:
                 self.player_pos.y -= self.displacement
             if keys[pygame.K_s]:
-                self.player_pos.y += self.displacement 
+                self.player_pos.y += self.displacement
             if keys[pygame.K_a]:
-                self.player_pos.x -= self.displacement 
+                self.player_pos.x -= self.displacement
             if keys[pygame.K_d]:
                 self.player_pos.x += self.displacement
 
+    def gesture_control(self):
+
+        mp_drawing = mp.solutions.drawing_utils
+        mp_drawing_styles = mp.solutions.drawing_styles
+        mp_hands = mp.solutions.hands
+
+        previousx = 0
+        previousy = 0
+        previousz = 0
+
+        # For webcam input:
+        self.cap = cv2.VideoCapture(0)
+        with mp_hands.Hands(
+                model_complexity=0,
+                min_detection_confidence=0.5,
+                min_tracking_confidence=0.5) as hands:
+            while self.running and self.cap.isOpened():
+                success, image = self.cap.read()
+                if not success:
+                    print("Ignoring empty camera frame.")
+                    # If loading a video, use 'break' instead of 'continue'.
+                    continue
+
+                # To improve performance, optionally mark the image as not writeable to
+                # pass by reference.
+                image.flags.writeable = False
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                results = hands.process(image)
+
+                # Draw the hand annotations on the image.
+                image.flags.writeable = True
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+                if results.multi_hand_landmarks:
+
+                    for hand_landmarks in results.multi_hand_landmarks:
+
+                        mp_drawing.draw_landmarks(
+                            image,
+                            hand_landmarks,
+                            mp_hands.HAND_CONNECTIONS,
+                            mp_drawing_styles.get_default_hand_landmarks_style(),
+                            mp_drawing_styles.get_default_hand_connections_style())
+                
+                    currentx = results.multi_hand_landmarks[0].landmark[8].x
+                    currenty = results.multi_hand_landmarks[0].landmark[8].y
+                    currentz = results.multi_hand_landmarks[0].landmark[8].z
+
+                    self.displacementx = (currentx-previousx)*1280
+                    self.displacementy = (currenty-previousy)*740
+                    print(self.displacementx)
+                    print(self.displacementy)
+
+                    previousx = currentx
+                    previousy = currenty
+
+                # Flip the image horizontally for a selfie-view display.
+                cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+                if cv2.waitKey(5) & 0xFF == 27:
+                    break
+        self.cap.release()
 
     def game_loop(self):
+
+        threading.Thread(
+            target=self.gesture_control, daemon=True
+        ).start()
+
+        clock = pygame.time.Clock()
+
         while self.running:
 
             for event in pygame.event.get():
@@ -35,16 +110,21 @@ class Movement_game():
                     self.running = False
 
                 self.screen.fill("purple")
-                pygame.draw.circle(self.screen,"red",self.player_pos, 20)
 
-                self.keyboard_movement()
+                self.player_pos.x += self.displacementx
+                self.player_pos.y += self.displacementy
+
+                pygame.draw.circle(self.screen, "red", self.player_pos, 20)
+                
+                self.keyboard_control()
+                # if self.cap.isOpened():
+                #     self.gesture_control()
 
                 pygame.display.flip()
 
+                clock.tick(30)
 
         pygame.quit()
-
-
 
 
 game = Movement_game()
